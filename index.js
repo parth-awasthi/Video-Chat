72eef3d7981ac154a63ef687f2bf78188d3fce42
Ragnar <parthawasthi13@gmail.com>
2025-02-28 17:23:22 +0530

    Updated websocket version to 4.8.1

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const { v4: uuidv4 } = require("uuid");
 app.set("view engine", "ejs");
 const io = require("socket.io")(server, {
   cors: {
-    origin: '*'
-  }
+    origin: '*',
+    methods: ["GET", "POST"],
+  },
+  transports: ['websocket', 'polling'],
 });
 const { ExpressPeerServer } = require("peer");
 const opinions = {
